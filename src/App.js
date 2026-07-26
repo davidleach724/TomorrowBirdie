ace08ec69c29de931bbc9bf1e24da5406e9c936f
David Leach <davidleach724@gmail.com>
2026-07-25 18:55:38 -0600

    Add redirect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import './App.css'
-import birdie from './birdie.JPG';
+import { useEffect } from "react";
+import "./App.css";
+import birdie from "./birdie.JPG";
 
 const App = () => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      window.location.href = "https://www.frnkastro.party";
+    }, 3000);
 
-    return (
-      <main>
-        <img src={birdie} alt="Birdie Parties" className="birdie-pic" />
-      </main>
-    )
-}
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <main>
+      <img src={birdie} alt="Birdie Parties" className="birdie-pic" />
+    </main>
+  );
+};
 
 export default App;
